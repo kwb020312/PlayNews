675f1ce9297226546aaa4f68aefa414d2d7bad86
seo sang kwon <seosangkwon@Jeongui-MacBookPro.local>
2020-09-14 10:28:49 +0900

    api 하나로 변경

--- api/news.js
+++ api/news.js
@@ -1,37 +1,14 @@
 import {
   article_url,
-  category,
   country_code,
   __api_key,
 } from "../config/rest_config";
 import axios from "axios";
 
-export const getArticles = async () => {
+export const getArticles = async (category) => {
   try {
     const articles = await axios.get(
       `${article_url}?country=${country_code}&category=${category}&apiKey=${__api_key}`
-    );
-    return articles.data.articles;
-  } catch (error) {
-    throw error;
-  }
-};
-
-export const getHealth = async () => {
-  try {
-    const articles = await axios.get(
-      `${article_url}?country=${country_code}&category=health&apiKey=${__api_key}`
-    );
-    return articles.data.articles;
-  } catch (error) {
-    throw error;
-  }
-};
-
-export const getEntertainment = async () => {
-  try {
-    const articles = await axios.get(
-      `${article_url}?country=${country_code}&category=entertainment&apiKey=${__api_key}`
     );
     return articles.data.articles;
   } catch (error) {
